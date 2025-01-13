0db8af47caa8e6ef88980e84f3fd9f7244a49823
jdebel2 <jdebel2@gmail.com>
2025-01-13 15:15:07 -0500

    First commit

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
+func main() {
+	commands = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+	getUserInput()
+}
+
+func cleanInput(text string) []string {
+	lowerString := strings.ToLower(text)
+	return strings.Fields(lowerString)
+}
+
+func getUserInput() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		userInput := scanner.Text()
+		cleanedInput := cleanInput(userInput)
+		commandString := cleanedInput[0]
+		command, ok := commands[commandString]
+		if ok {
+			command.callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
+	for _, value := range commands {
+		fmt.Printf("%s: %s\n", value.name, value.description)
+	}
+	return nil
+}
